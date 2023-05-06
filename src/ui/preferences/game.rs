8007270f48b0f5031eaa24f0865ee15a29c1d2e5
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2023-05-06 15:27:56 +0200

    fix: fixed merge issues

--- src/ui/preferences/game.rs
+++ src/ui/preferences/game.rs
@@ -5,7 +5,7 @@ use relm4::factory::*;
 use adw::prelude::*;
 
 use anime_launcher_sdk::sessions::SessionsExt;
-use anime_launcher_sdk::genshin::sessions::Sessions;
+use anime_launcher_sdk::star_rail::sessions::Sessions;
 
 use super::main::PreferencesAppMsg;
 

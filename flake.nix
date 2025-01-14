a76339feb7eac04f56e7f29b5980bd54bd5f381d
Nikita Podvirnyi <krypt0nn@vk.com>
2025-01-14 16:28:04 +0200

    1.10.0

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
         nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
